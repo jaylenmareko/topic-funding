abe0e6f8f10858e4ba2e327f45081c98259df686
jaylenmareko <marekodavis@gmail.com>
2025-07-13 08:34:40 -0600

    Update stripe.php

--- config/stripe.php
+++ config/stripe.php
@@ -6,6 +6,9 @@ require_once __DIR__ . '/../stripe-php/init.php';
 define('STRIPE_PUBLISHABLE_KEY', 'pk_live_51QbKG8Kg5kxyA3jRmQrkUtkB4OKH0xwvAlpm5huKGKKh1G9QbLJYP8ZMijil8f3oQWHuDWofc0FRmapwzfqhw9Ne00eDEcgBQ3');
 define('STRIPE_SECRET_KEY', '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');
 
+// ADD THIS LINE - Get webhook secret from Stripe Dashboard > Webhooks > Your endpoint > Signing secret
+define('STRIPE_WEBHOOK_SECRET', 'whsec_kDLBRQ8VwuhVYPr3nreQOQKZJXaBpytv');
+
 \Stripe\Stripe::setApiKey(STRIPE_SECRET_KEY);
 
 define('STRIPE_CURRENCY', 'usd');
