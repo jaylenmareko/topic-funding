10f6d2196a2d48f728521f143b306002ca53dc24
jaylenmareko <marekodavis@gmail.com>
2026-01-14 20:36:30 -0600

    Update dashboard.php

--- creators/dashboard.php
+++ creators/dashboard.php
@@ -620,12 +620,6 @@ foreach ($topics as $topic) {
                     </svg>
                     Edit Profile
                 </a>
-                <a href="../auth/logout.php" class="btn">
-                    <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
-                        <path d="M9 21H5a2 2 0 01-2-2V5a2 2 0 012-2h4M16 17l5-5-5-5M21 12H9"></path>
-                    </svg>
-                    Log Out
-                </a>
             </div>
         </div>
 
