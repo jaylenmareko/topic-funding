a9f78747f0bcc544ac09b28ab695662c4a4e499a
jaylenmareko <marekodavis@gmail.com>
2026-01-30 17:36:15 -0600

    Create stripe-keys.php

---
+++ config/stripe-keys.php
@@ -0,0 +1,6 @@
+<?php
+// config/stripe-keys.php - Stripe API Keys (DO NOT COMMIT TO GIT)
+define('STRIPE_PUBLISHABLE_KEY', 'pk_live_51SigZ2FBOm4auH4Pqh0Td844HNjiTKa1GTRJsRMYog1dEhTm8yzqoVZdyZgLEqhRjqx7zBjPjWG5UOaNvs2ZfVE900yfpDwIvd');
+define('STRIPE_SECRET_KEY', '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');
+define('STRIPE_WEBHOOK_SECRET', '');
+?>
