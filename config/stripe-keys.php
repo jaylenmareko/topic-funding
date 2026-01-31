f0e671f2b933c912fef59dd74f34b58026016a8a
jaylenmareko <marekodavis@gmail.com>
2026-01-30 23:38:03 -0600

    Update stripe-keys.php

--- config/stripe-keys.php
+++ config/stripe-keys.php
@@ -1,6 +1,6 @@
 <?php
 // config/stripe-keys.php - Stripe API Keys (DO NOT COMMIT TO GIT)
-define('STRIPE_PUBLISHABLE_KEY', 'pk_live_51SigZ2FBOm4auH4Pqh0Td844HNjiTKa1GTRJsRMYog1dEhTm8yzqoVZdyZgLEqhRjqx7zBjPjWG5UOaNvs2ZfVE900yfpDwIvd');
+define('STRIPE_PUBLISHABLE_KEY', 'pk_live_51M6chXKzDw80HjwVFOb94yJYeJE3OYymfTDWzbldf4NMmRi9x4NZOMD3Dw2mYXr8rSQPwR5gJcOABq2iEjCGVaGe00oXa6x1lD');
 define('STRIPE_SECRET_KEY', '***REMOVED***');
-define('STRIPE_WEBHOOK_SECRET', '');
+define('STRIPE_WEBHOOK_SECRET', 'whsec_OWYNjRJeZ56UDvJobQoVIfy4Fzu9g7rl');
 ?>
